33efe3f19a4b553b908fb3e575345d9dff49bc8d
Yadickson Soto <yadickson@gmail.com>
2025-02-10 12:22:04 -0300

    Fix constraint and models data

--- postgresql/1.0.0/datasets/models.sql
+++ postgresql/1.0.0/datasets/models.sql
@@ -4,6 +4,6 @@
 --
 -- Table: models
 
-insert into models (code, category, created_at, created_by, enabled, updated_at, updated_by) values ('DORMANT', ANOMALY_DETECTION, 2024-05-27 15:31:03.49615, 'me', true, 2024-05-29 11:16:05.075389, 'me');
-insert into models (code, category, created_at, created_by, enabled, updated_at, updated_by) values ('NEWLY_SEEN', ANOMALY_DETECTION, 2024-05-27 15:31:03.49615, 'me', true, 2024-05-29 11:16:05.075389, 'me');
-insert into models (code, category, created_at, created_by, enabled, updated_at, updated_by) values ('PASS_THROUGH', STRUCTURING, 2024-05-27 22:35:59.571934, 'me', true, 2024-05-29 11:16:05.075389, 'me');
+insert into models (code, category, created_at, created_by, enabled, updated_at, updated_by) values ('DORMANT', 'ANOMALY_DETECTION'::t_model_category, '2024-05-27 15:31:03.49615', 'me', true, '2024-05-29 11:16:05.075389', 'me');
+insert into models (code, category, created_at, created_by, enabled, updated_at, updated_by) values ('NEWLY_SEEN', 'ANOMALY_DETECTION'::t_model_category, '2024-05-27 15:31:03.49615', 'me', true, '2024-05-29 11:16:05.075389', 'me');
+insert into models (code, category, created_at, created_by, enabled, updated_at, updated_by) values ('PASS_THROUGH', 'STRUCTURING'::t_model_category, '2024-05-27 22:35:59.571934', 'me', true, '2024-05-29 11:16:05.075389', 'me');
